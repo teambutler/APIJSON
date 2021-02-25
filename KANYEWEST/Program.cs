6cc68b64e39894fd10d796e1088f6f1da7aca3fc
David Butler <butler.david1906@gmail.com>
2021-02-25 15:40:17 -0500

    update

---
+++ KANYEWEST/Program.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace KANYEWEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuoteGenerator.KANYEQUOTE();
+
+            QuoteGenerator.RonQuote();
+        }
+    }
+}
---
+++ KANYEWEST/QuoteGenerator.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace KANYEWEST
+{
+    public class QuoteGenerator
+    {
+        public static void KANYEQUOTE()
+        {
+            var client = new HttpClient();
+
+            var kayneURL = "https://api.kanye.rest";
+
+            var kanyeResponse = client.GetStringAsync(kayneURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            Console.WriteLine($"---------------------------------");
+            Console.WriteLine($"Kanye: '{kanyeQuote}'");
+            Console.WriteLine($"---------------------------------");
+        }
+        public static void RonQuote()
+        {
+            var client = new HttpClient();
+
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes;
+
+            var ronResponse = client.GetStringAsync(ronURL).Result;
+
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            Console.WriteLine($"---------------------------------");
+            Console.WriteLine($"Ron: '{ronQuote}'");
+            Console.WriteLine($"---------------------------------");
+        }
+   }
+}
